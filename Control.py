615360e34dc02c8f2a6ca2c0cbd474e4d33da3a1
Egor-cmd-alt <63338258+Egor-cmd-alt@users.noreply.github.com>
2020-04-11 13:39:38 +0300

    Update Control.py

--- Control.py
+++ Control.py
@@ -7,7 +7,7 @@ x = Model.meX
 
 def move(canvas, a, b):
     global x, y
-    if Model.maze[x + a][y + b] == 1 or x+a>19 or x+a<0 or y+a>19  or y+a<0:
+    if Model.maze[x + a][y + b] == 1 or x+a>19 or x+a<0 or y+b>19  or y+b<0:
         x += 0
         y += 0
     elif Model.maze[x + a][y + b] == 0 or Model.maze[x + a][y + b] == 2:
