6ae61d1b3d99ec28a20c14f9ce0657dd1e079bd9
Egor-cmd-alt <63338258+Egor-cmd-alt@users.noreply.github.com>
2020-04-15 13:56:08 +0300

    Update View.py

--- View.py
+++ View.py
@@ -22,12 +22,28 @@ for i in range(Model.size):
             color = 'blue'
         canvas.create_rectangle(r * i + 100, r * j + 100, r * i + r + 100, r * j + r + 100, fill=color)
 
+def f():
+    global n
+    for i in Model.maze:
+        for j in Model.maze:
+            if j == 3:
+                Model.maze[i].insert(j, 0)
+                canvas.create_rectangle(r * i + 100, r * j + 100, r * i + r + 100, r * j + r + 100, fill='blue')
+                j += 1
+                Model.maze[i].insert(j, 3)
+                canvas.create_rectangle(r * i + 100, r * j + 100, r * i + r + 100, r * j + r + 100, fill='yellow')
+    window.after(1000, lambda: (f()))
+    n += 1
+
+n = 1
+while n != 16:
+    f()
+
+
 window.bind('<Up>', lambda event: Control.move(canvas, 0, -1))
 window.bind('<Down>', lambda event: Control.move(canvas, 0, 1))
 window.bind('<Left>', lambda event: Control.move(canvas, -1, 0))
 window.bind('<Right>', lambda event: Control.move(canvas, 1, 0))
 
-def f():
-    pass
-f()
+
 window.mainloop()
