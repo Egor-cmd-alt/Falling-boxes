d9605641f457fbddff644a4f0e6753eec076a80d
Egor-cmd-alt <63338258+Egor-cmd-alt@users.noreply.github.com>
2020-05-08 13:44:16 +0300

    Update Control.py

--- Control.py
+++ Control.py
@@ -7,15 +7,11 @@ x = Model.meX
 
 def move(canvas, a, b):
     global x, y
-    if Model.maze[x + a][y + b] == 1 or x+a>19 or x+a<0 or y+b>19  or y+b<0:
+    if Model.maze[x][y] == 1 or x>19 or x<0 or y>19 or y<0:
         x += 0
         y += 0
-    elif Model.maze[x + a][y + b] == 0 or Model.maze[x + a][y + b] == 2:
-        canvas.create_rectangle(r * x + 100, r * y + 100, r * x + r + 100, r * y + r + 100, fill='blue')
-        x += a
-        y += b
-        canvas.create_rectangle(r * x + 100, r * y + 100, r * x + r + 100, r * y + r + 100, fill='pink')
+    elif Model.maze[x][y] == 0 or Model.maze[x][y] == 2:
+        canvas.move('i',a,b)
     elif Model.maze[x + a][y + b] == 3:
         x+=0
         y+=0
-
