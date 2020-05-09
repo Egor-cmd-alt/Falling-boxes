2b8f2a5f0e785222885ff3725a97e6cd23f51043
Egor-cmd-alt <63338258+Egor-cmd-alt@users.noreply.github.com>
2020-05-09 17:43:55 +0300

    Update Model.py

--- Model.py
+++ Model.py
@@ -3,19 +3,32 @@ import random
 size = 20
 meX = random.randint(1, 10)
 meY = 16
-bX=[random.randint(1, 10)]
+bX=random.randint(1, 10)
 bY = 1
 maze = []
 for i in range(size):
     maze.append([])
     for j in range(size):
-        if i == meX and j == meY:
-            maze[i].append(2)
-        elif i == bX[0] and j == bY:
-            maze[i].append(3)
-        elif j >16:
+        if j >16:
             maze[i].append(1)
         else:
             maze[i].append(0)
-
+me=[]
+for i in range(size):
+    me.append([])
+    for j in range(size):
+        if i == meX and j == meY:
+            me[i].append(2)
+        else:
+            me[i].append(0)
+box=[]
+for i in range(size):
+    box.append([])
+    for j in range(size):
+        if i == bX and j == bY:
+            box[i].append(3)
+        else:
+            box[i].append(0)
 print(maze)
+print(me)
+print(box)
