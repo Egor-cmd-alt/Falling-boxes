4615b29617a51eded3adb053ccff6d9a23977da0
Egor-cmd-alt <63338258+Egor-cmd-alt@users.noreply.github.com>
2020-05-09 17:42:51 +0300

    Update View.py

--- View.py
+++ View.py
@@ -3,7 +3,7 @@ import Control
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry("1300x700")
+window.geometry("1200x700")
 window.resizable(True, True)
 window.title("Падающие ящики")
 
@@ -12,27 +12,39 @@ canvas.pack()
 r = 30
 for i in range(Model.size):
     for j in range(Model.size):
-        if Model.maze[i][j] == 1:
+        if Model.box[i][j] == 3:
+            color = 'yellow'
+            tag='box'
+        elif Model.me[i][j] == 2:
+            color = 'pink'
+            tag = 'i'
+        elif Model.maze[i][j] == 1:
             color = 'brown'
             tag='ground'
         elif Model.maze[i][j] == 0:
-            color = 'blue'
             tag='fon'
-        elif Model.maze[i][j] == 3:
-            color = 'yellow'
-            tag='box'
-        else:
-            color = 'pink'
-            tag = 'i'
+            color = None
+
 
         canvas.create_rectangle(r * i + 100, r * j + 100, r * i + r + 100, r * j + r + 100, fill=color, tag=tag)
 
+dx=0
+dy=1
+
+
 def move(canvas,a,b):
     Control.move(canvas,a,b)
+    print(canvas.coords('i'))
+
+def go(dx,dy):
+        canvas.move("box", dx, dy)
 
 
+def f():
+    go(dx, dy)
+    window.after(50, lambda: f())
 
-
+f()
 
 
 
@@ -40,8 +52,8 @@ def move(canvas,a,b):
 
 window.bind('<Up>', lambda event: move(canvas, 0, -r))
 window.bind('<Down>', lambda event: move(canvas, 0, r))
-window.bind('<Left>', lambda event: move(canvas, -r, 0))
-window.bind('<Right>', lambda event: move(canvas, r, 0))
+window.bind('<Left>', lambda event: move(canvas, -5, 0))
+window.bind('<Right>', lambda event: move(canvas, 5, 0))
 
 
 window.mainloop()
